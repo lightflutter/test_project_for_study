1603b6d0ca87f358095d1c296ff4931a414132f4
lightflutter <iamwaizord@gmail.com>
2023-04-21 23:59:38 +0300

    Add: Add new class HintLabel

---
+++ lib/HintLabel.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HintLabel extends StatelessWidget {
+  final String text;
+
+  const HintLabel(this.text);
+
+  @override
+  Widget build(BuildContext context) {
+    return DecoratedBox(
+      decoration: BoxDecoration(color: Colors.amber[200]),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(text,
+          style: TextStyle(color: Colors.grey[700])),
+      ),
+    );
+  }
+}
