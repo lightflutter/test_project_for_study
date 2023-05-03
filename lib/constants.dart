9dc3ccf36918c2d94bb3d93cfed53e5350545997
lightflutter <iamwaizord@gmail.com>
2023-05-03 12:23:26 +0300

    docx: Add new class Constants for URL request

---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+class Constants {
+  static const String WEATHER_BASE_SCHEME = 'https://';
+  static const String WEATHER_BASE_URL = 'api.openweathermap.org';
+  static const String WEATHER_IMAGE_PATH = '/img/w/';
+  static const String WEATHER_IMAGE_URL = WEATHER_BASE_SCHEME + WEATHER_BASE_URL + WEATHER_IMAGE_PATH;
+  static const String WEATHER_FORECAST_URL = '/data/2.5/forecast';
+  static const String WEATHER_APP_ID = 'bf78f267cdb48d5bae47166f91e8122f';
+}
