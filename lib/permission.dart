b6a0209b00803501274f8211fafe342fe0a441df
lightflutter <iamwaizord@gmail.com>
2023-07-10 17:09:42 +0300

    feat: Add method for get user permission

--- lib/permission.dart
+++ lib/permission.dart
@@ -1,43 +1,22 @@
 import 'package:geolocator/geolocator.dart';
-class Permission {
-  check() async{
-    LocationPermission permission;
-    permission = await Geolocator.checkPermission();
-    print('$permission');
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.deniedForever) {
-        return Future.error('Location Not Available');
-      }
-    } else {
-      throw Exception('Error');
-    }
-  }
 
-  Future<bool> _handleLocationPermission() async {
+class Permission {
+  Future<bool> handleLocationPermission() async {
     bool serviceEnabled;
     LocationPermission permission;
 
     serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text(
-              'Location services are disabled. Please enable the services')));
       return false;
     }
     permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: Text('Location permissions are denied')));
         return false;
       }
     }
     if (permission == LocationPermission.deniedForever) {
-      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-          content: Text(
-              'Location permissions are permanently denied, we cannot request permissions.')));
       return false;
     }
     return true;
