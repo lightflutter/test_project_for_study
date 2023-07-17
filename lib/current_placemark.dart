c29c608ded65355bc7555efc43155b20c40d484c
lightflutter <iamwaizord@gmail.com>
2023-07-17 23:37:08 +0300

    fix: Add new method to class CurrentPlacemark

--- lib/current_placemark.dart
+++ lib/current_placemark.dart
@@ -1,11 +1,26 @@
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:test_project_for_study/permission.dart';
 
 class CurrentPlacemark {
+  final Permission _permission = Permission();
+
+  Future<Placemark> getLocation() async {
+    CurrentPlacemark currentPlacemark = CurrentPlacemark();
+    bool hasPermission = await _permission.handleLocationPermission();
+    print('$hasPermission');
+    if (!hasPermission) {
+      print('Hasn\'t permission ${Placemark()}');
+      return Placemark();
+    } else {
+      return await currentPlacemark.get();
+    }
+  }
+
   Future<Placemark> get() async {
     Position position = await getPosition();
     List<Placemark> placemark =
-    await placemarkFromCoordinates(position.latitude, position.longitude);
+        await placemarkFromCoordinates(position.latitude, position.longitude);
 
     if (placemark.isNotEmpty) {
       print('GOOD REQUEST ${placemark[0]}');
