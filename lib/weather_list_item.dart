8bc8893966a0f3f30674ecc9b126f515b59c9231
lightflutter <iamwaizord@gmail.com>
2023-07-28 20:28:17 +0300

    Fix: Change class WeatherListItem for new Data from json

--- lib/weather_list_item.dart
+++ lib/weather_list_item.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:test_project_for_study/weather.dart';
+import 'package:test_project_for_study/model/forecast_entity.dart';
 
 class WeatherListItem extends StatelessWidget {
-  static final _dateFormat = DateFormat('yyyy-MM-dd - hh:mm');
-  final Weather weather;
+  static const String _weatherURL = 'http://openweathermap.org/img/w/';
+  static final _dateFormatTime = DateFormat('HH:mm');
+  final ForecastList forecastList;
 
-  const WeatherListItem(this.weather, {Key? key}) : super(key: key);
+  const WeatherListItem(this.forecastList, {Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(flex: 3, child: Text(_dateFormat.format(weather.dateTime))),
-          Expanded(flex: 1, child: Text('${weather.degree} C')),
-          Expanded(flex: 1, child: Text('${weather.clouds}')),
-          Expanded(flex: 1, child: Image.network(weather.getIconURL()))
+          Padding(
+            padding: const EdgeInsets.only(right: 16.0),
+            child: Text(
+                _dateFormatTime.format(
+                    DateTime.fromMillisecondsSinceEpoch(forecastList.dt)),
+                style: Theme.of(context).textTheme.labelLarge),
+          ),
+          Image.network('$_weatherURL${forecastList.weather.first.icon}'),
+          Text('${forecastList.main.temp.toInt()}\u00B0C',
+              style: Theme.of(context).textTheme.headlineLarge),
         ],
       ),
     );
